796cb538076a54437d79790d0647aa0ec51476a5
raman.bachyla <raman.bachyla@revelsystems.com>
2025-02-18 22:26:02 -0800

    remove redundant variables, add constistant quotes

--- news/pytests/test_routes.py
+++ news/pytests/test_routes.py
@@ -7,75 +7,73 @@ pytestmark = pytest.mark.django_db
 
 
 @pytest.mark.parametrize(
-    "client_fixture, url, expected_status",
+    'client_fixture, url, expected_status',
     (
         # Public endpoints
         (
-            pytest.lazy_fixture("client"),
-            pytest.lazy_fixture("home_page_url"),
+            pytest.lazy_fixture('client'),
+            pytest.lazy_fixture('home_page_url'),
             HTTPStatus.OK
         ),
         (
-            pytest.lazy_fixture("client"),
-            pytest.lazy_fixture("news_detail_url"),
+            pytest.lazy_fixture('client'),
+            pytest.lazy_fixture('news_detail_url'),
             HTTPStatus.OK
         ),
         (
-            pytest.lazy_fixture("client"),
-            pytest.lazy_fixture("login_page_url"),
+            pytest.lazy_fixture('client'),
+            pytest.lazy_fixture('login_page_url'),
             HTTPStatus.OK
         ),
         (
-            pytest.lazy_fixture("client"),
-            pytest.lazy_fixture("logout_page_url"),
+            pytest.lazy_fixture('client'),
+            pytest.lazy_fixture('logout_page_url'),
             HTTPStatus.OK
         ),
         (
-            pytest.lazy_fixture("client"),
-            pytest.lazy_fixture("signup_page_url"),
+            pytest.lazy_fixture('client'),
+            pytest.lazy_fixture('signup_page_url'),
             HTTPStatus.OK
         ),
         # Protected endpoints
         (
-            pytest.lazy_fixture("author_client"),
-            pytest.lazy_fixture("comment_edit_url"),
+            pytest.lazy_fixture('author_client'),
+            pytest.lazy_fixture('comment_edit_url'),
             HTTPStatus.OK
         ),
         (
-            pytest.lazy_fixture("not_author_client"),
-            pytest.lazy_fixture("comment_edit_url"),
+            pytest.lazy_fixture('not_author_client'),
+            pytest.lazy_fixture('comment_edit_url'),
             HTTPStatus.NOT_FOUND
         ),
         (
-            pytest.lazy_fixture("author_client"),
-            pytest.lazy_fixture("comment_delete_url"),
+            pytest.lazy_fixture('author_client'),
+            pytest.lazy_fixture('comment_delete_url'),
             HTTPStatus.OK
         ),
         (
-            pytest.lazy_fixture("not_author_client"),
-            pytest.lazy_fixture("comment_delete_url"),
+            pytest.lazy_fixture('not_author_client'),
+            pytest.lazy_fixture('comment_delete_url'),
             HTTPStatus.NOT_FOUND
         ),
     ),
 )
 def test_status_codes(client_fixture, url, expected_status):
-    response = client_fixture.get(url)
-    assert response.status_code == expected_status
+    assert client_fixture.get(url).status_code == expected_status
 
 
 @pytest.mark.parametrize(
-    "url, expected_url",
+    'url, expected_url',
     (
         (
-            pytest.lazy_fixture("comment_edit_url"),
-            pytest.lazy_fixture("login_to_comment_edit_url")
+            pytest.lazy_fixture('comment_edit_url'),
+            pytest.lazy_fixture('login_to_comment_edit_url')
         ),
         (
-            pytest.lazy_fixture("comment_delete_url"),
-            pytest.lazy_fixture("login_to_comment_delete_url")
+            pytest.lazy_fixture('comment_delete_url'),
+            pytest.lazy_fixture('login_to_comment_delete_url')
         ),
     ),
 )
 def test_redirects(client, url, expected_url):
-    response = client.get(url)
-    assertRedirects(response, expected_url)
+    assertRedirects(client.get(url), expected_url)
